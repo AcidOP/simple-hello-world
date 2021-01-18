daae1dc52cac6ecd6fddcf0d1c3a6a2f16e4c3c4
acidOP <67377785+acidOP@users.noreply.github.com>
2021-01-18 15:51:35 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,32 +1,51 @@
-import string
-from time import sleep
-from random import choice
+from time import *
+from random import *
 
-
-class hello:
-
+class Class:
     def __init__(self):
-        self.hello = ['H', 'E', 'L', 'L', 'O', 'W', 'O', 'R', 'L', 'D']
-        self.word = ''
+        self.hello = [72, 69, 76, 76, 79, 87, 79, 82, 76, 68]
+        self.dontKnowWhatTheFuckArray = []
+        self.big_A_to_big_Z_ascii = []
+        self.big_A_to_big_Z = []
+        self.xx = 65
+        self.xxx = 90
+        self.timer = 0.5
+        self.result = ''
+        self.display = ''
+        self.count = 0
+        self.check = True
+        self.update_big_A_to_big_Z()
+        self.ascii_to_letter()
 
-    def generate(self):
-        throw = ''.join(choice(string.ascii_uppercase))
-        return throw
+    def update_big_A_to_big_Z(self):
+        for i in range(self.xx, self.xxx+1):
+            self.dontKnowWhatTheFuckArray.append(i)
+        self.big_A_to_big_Z_ascii.extend(self.dontKnowWhatTheFuckArray)
+
+    def ascii_to_letter(self):
+        for integer in self.big_A_to_big_Z_ascii:
+            self.big_A_to_big_Z.append(chr(integer))
 
     def takeLetter(self):
-        i = 0
-        while True:
-            sleep(0.5)
-            self.letter = hello.generate(self)
-            print(f'\n\t {self.letter}')
+        takeYourCHance = ''.join(choice(self.big_A_to_big_Z))
+        return takeYourCHance
 
-            if (self.letter == self.hello[i]):
-                self.word += self.letter
-                print(f'\n\t {self.word}')
-                i += 1
-                if (self.word == 'HELLOWORLD'):
-                    return False
-                
-        
-xxx = hello()
-xxx.takeLetter()
+    def bruteForce(self):
+        while self.check:
+            sleep(self.timer)
+            takeYourChanceAgain = Class.takeLetter(self)
+            print(takeYourChanceAgain)
+
+            if (takeYourChanceAgain == chr(self.hello[self.count])):
+                self.display += takeYourChanceAgain
+                self.display += ' '
+                self.result += takeYourChanceAgain
+                print(self.display)
+                self.count += 1
+
+                if (self.result == 'HELLOWORLD'):
+                    self.check = False
+                    return self.check
+
+xxx = Class()
+xxx.bruteForce()
